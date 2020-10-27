b8fc56b4eecd7aaaa90d6f65b9ca1c093cb125ee
Jae-Hyuck Park <jaehyuck0103@gmail.com>
2020-10-27 20:49:39 +0900

    taillight2 common.cpp 내용을 hpp로 통합시켜버림.

--- apps/taillight2/common.hpp
+++ apps/taillight2/common.hpp
@@ -5,14 +5,27 @@
 
 typedef Eigen::Matrix<bool, Eigen::Dynamic, Eigen::Dynamic> MatrixXXb;
 
+// clang-format off
 struct CalibParams {
-    static const Eigen::Matrix4f RT;
-    static const Eigen::Matrix4f RTinv;
-    static const Eigen::Matrix3f K;
+    inline static const Eigen::Matrix4f RT = (
+            Eigen::Matrix4f() << -0.005317, 0.003402, 0.999980, 1.624150,
+                                 -0.999920, -0.011526, -0.005277, 0.296660,
+                                 0.011508, -0.999928, 0.003463, 1.457150,
+                                 0, 0, 0, 1
+            ).finished();
+    inline static const Eigen::Matrix4f RTinv = RT.inverse();
+    inline static const Eigen::Matrix3f K = (
+            Eigen::Matrix3f() << 819.162645, 0.000000, 640.000000,
+                                 0.000000, 819.162645, 240.000000,
+                                0.000000, 0.000000, 1.000000
+            ).finished();
 };
+// clang-format on
 
 // angleDiff (-pi, pi)
-float angleDiff(float toAngle, float fromAngle);
+inline float angleDiff(float toAngle, float fromAngle) {
+    return remainder((toAngle - fromAngle), 2 * M_PI);
+}
 
 namespace Cfg {
 constexpr int batchSize = 8;
